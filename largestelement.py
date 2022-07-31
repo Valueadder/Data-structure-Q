11f2707b095e61d1d42a927c46b0ea6576aa1f10
Manoj Nahak <nahakmanoj124@gmail.com>
2022-07-31 14:23:16 +0530

    Optimized solution of finding largest element

--- largestelement.py
+++ largestelement.py
@@ -1,12 +1,21 @@
 def largestelement(l):
-  for x in l:
-    for y in l:
-      if y>x:
-        break
-    else:
-      return x
-  return None
+   if not l:
+     return None
+   else:
+     result=l[0]
+     for i in range(1,len(l)):
+       if l[i]>result:
+         result=l[i]
+   return result
+#   for x in l:
+#     for y in l:
+#       if y>x:
+#         break
+#     else:
+#       return x
+#   return None
 
 if __name__== "__main__":
   list1=list(map(int, input().split(' ')))   
-  print(largestelement(list1))     
+  print(largestelement(list1))   
+
