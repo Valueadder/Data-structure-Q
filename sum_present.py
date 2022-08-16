a10b5650b2d6c6f2926309fe43613a0a1b848810
Manoj Nahak <nahakmanoj124@gmail.com>
2022-08-16 15:54:08 +0530

    optimized solution for finding max element in array using sliding window technique

--- sum_present.py
+++ sum_present.py
@@ -1,17 +1,28 @@
 # In this program we are going to find max sum with k given elements
 # Naive solution
-def maxk(arr, k):
-    res = float("-inf")
-    i = 0
-    n = len(arr)
-    while(i+k-1 < n):
-        curr = 0
-        for j in range(k):
-            curr += arr[i+j]
-        res = max(curr, res)
-        i = i+1
-    return res
+# def maxk(arr, k):
+#     res = float("-inf")
+#     i = 0
+#     n = len(arr)
+#     while(i+k-1 < n):
+#         curr = 0
+#         for j in range(k):
+#             curr += arr[i+j]
+#         res = max(curr, res)
+#         i = i+1
+#     return res
 
+# optimized solution using sliding window technique
+def maxk(arr,k):
+  curr=0
+  n=len(n)
+  for i in range(k):
+    curr+=arr[i]
+  res=curr
+  for j in range(k, n):
+    curr= curr+ arr[i]- arr[i-k]
+    res=max(curr, res)
+  return res 
    
 
 
