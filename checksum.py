59f8580b22537c5eff6f0951bd97a05056074e5a
Manoj Nahak <nahakmanoj124@gmail.com>
2022-08-25 10:46:23 +0530

    applied sliding window technique

--- checksum.py
+++ checksum.py
@@ -10,10 +10,20 @@
 #   return False
 
 #optimized solution
-#   
+def subsum(arr, sume):
+  curr = 0
+  i= 0
+  for e in range(len(arr)):
+    curr+=arr[e]
+    while (curr>sume):
+      curr-=arr[i]
+      i+=1 
+    if curr==sume:
+      return True
+  return False       
 
 if __name__== "__main__":
   l=[2, 4, 5, 6, 7]
-  k=9
+  k=23
   v=subsum(l, k)
   print(v)       
