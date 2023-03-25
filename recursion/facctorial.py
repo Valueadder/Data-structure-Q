b3aeeeb3de64a4cb88bdd8d7691da0a60e57e33d
Manoj Nahak <nahakmanoj124@gmail.com>
2023-03-25 16:01:45 +0530

    recursive program to find factorial of a number

---
+++ recursion/facctorial.py
@@ -0,0 +1,16 @@
+# program to find factorial using non tail recursive function
+# def fact(n):
+#     if n <= 0:
+#         return 1
+#     return n*fact(n-1)
+
+# program to find factorial of a number using tail recursive function
+
+# def fact(n, a):
+#     if n == 0:
+#         return a
+#     return fact(n-1, n*a)
+
+
+# n = int(input("Enter the number to find factorial:\n"))
+# print(fact(n, 1))
